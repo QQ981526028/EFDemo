cb531038fe845e4c079a7d581b83cdd900063919
刘俊宏 <981526028@qq.com>
2018-12-17 22:48:18 +0800

    测试来自数据库的CodeFirst，创建实体模型

---
+++ EFDemo/CodeFirstDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KTStoreModel model = new KTStoreModel();
+            int c = model.Product.Count();
+            Console.ReadKey();
+        }
+    }
+}
